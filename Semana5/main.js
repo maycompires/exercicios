18022eb895cc20bab8a80cb13d9e19b616777db4
maycompires <maycompires@hotmail.com>
2024-02-15 21:43:09 -0300

    estrutura while

--- Semana5/main.js
+++ Semana5/main.js
@@ -8,27 +8,28 @@ A execução do algoritmo deve ser contínua e o número 0 ou ‘sair’
 
 const consultaProduto = prompt("Digite o nome ou código do alimento ('0'ou 'sair' para encerrar Programa):");
 
-let continua = false;
+let continua = true;
 
-while (continua){
-switch (consultaProduto.toLocaleUpperCase()) {
-    case 'Banana':
-    case '01':
-        alert("O Valor da Banana é R$ 5,99");
-        break;
-    case 'Abacate':
-    case '02':
-        alert("O Valor da Abacate é R$ 8,99");
-        break;
-    case 'Abacaxi':
-    case '03':
-        alert("O Valor da Abacaxi é R$ 6,99");
-        break;
+while (continua) {
+    switch (consultaProduto.toLocaleUpperCase()) {
+        case 'Banana':
+        case '01':
+            alert("O Valor da Banana é R$ 5,99");
+            break;
+        case 'Abacate':
+        case '02':
+            alert("O Valor da Abacate é R$ 8,99");
+            break;
+        case 'Abacaxi':
+        case '03':
+            alert("O Valor da Abacaxi é R$ 6,99");
+            break;
         case 'sair':
         case '0':
-        alert("Para Encerrar o Programa");
-        break;
-    default:
-        alert("Nome ou Código do Produto é Inválido");
-}
+            alert("Programa Encerrado");
+            continua = false
+            break;
+        default:
+            alert("Nome ou Código do Produto é Inválido");
+    }
 }
