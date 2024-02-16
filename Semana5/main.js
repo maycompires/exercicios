bc2db30a110635b1f4261a12b5b29cca3a62f507
maycompires <maycompires@hotmail.com>
2024-02-15 21:20:19 -0300

    mudança console.lo para alert

--- Semana5/main.js
+++ Semana5/main.js
@@ -11,16 +11,16 @@ const consultaProduto = prompt("Digite o Nome ou Código do Produto");
 switch (consultaProduto) {
     case 'Banana':
     case '01':
-        console.log("O Valor da Banana é R$ 5,99");
+        alert("O Valor da Banana é R$ 5,99");
         break;
     case 'Abacate':
     case '02':
-        console.log("O Valor da Abacate é R$ 8,99");
+        alert("O Valor da Abacate é R$ 8,99");
         break;
     case 'Abacaxi':
     case '03':
-        console.log("O Valor da Abacaxi é R$ 6,99");
+        alert("O Valor da Abacaxi é R$ 6,99");
         break;
     default:
-        console.log("Nome ou Código do Produto é Inválido");
+        alert("Nome ou Código do Produto é Inválido");
 }
