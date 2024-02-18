b982e407da31758731991abf986b4c4bf957b988
maycompires <maycompires@hotmail.com>
2024-02-18 19:36:32 -0300

    exercicio7

--- Semana4/ex07/main.js
+++ Semana4/ex07/main.js
@@ -1,15 +1,3 @@
-/*function saudacao(horaAtual) {
-    if (horaAtual >= 0 && horaAtual < 12){
-    console.log("Olá Bom dia")
-    }else if (horaAtual >= 12 && horaAtual < 18){
-      console.log("Olá Boa Tarde")
-    }else{
-      console.log("Olá Boa noite")
-    }
-  }
-  
-  let horaAtual = document.getElementById('time').value
-  Console.log(horaAtual)*/
   function saudacao() {
     let horaAtual = document.getElementById('time').value;
     let hora = parseInt(horaAtual.split(':')[0]); // Obtém a hora do campo de entrada
