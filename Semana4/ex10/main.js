f708151bb5924dec71b33572bf7c4754995b923c
maycompires <maycompires@hotmail.com>
2024-02-18 23:02:39 -0300

    exercício 10

---
+++ Semana4/ex10/main.js
@@ -0,0 +1,10 @@
+function verificarMaiorIdade (){
+    let idade = prompt("Digite sua Idade")
+    if(idade >= 18){
+        console.log("Você é Maior de Idade")
+    }else{
+        console.log("Você é Menor de Idade")
+    }
+}
+
+verificarMaiorIdade()
