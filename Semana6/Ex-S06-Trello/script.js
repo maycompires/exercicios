5053ac942dae68c5af0fb76183d1fe9a839442e1
Maycom Pires <maycompires@hotmail.com>
2024-02-27 21:46:06 -0300

    Ex 8 - Manipulação do DOM

--- Semana6/Ex-S06-Trello/script.js
+++ Semana6/Ex-S06-Trello/script.js
@@ -1,3 +1,17 @@
 function exibirMensagem() {
-    window.alert("Aqui Você Pode entrar em Contato Comigo!");
+    window.alert("Aqui Você Pode entrar em Contato com Maycom.");
 }
+
+function solicitarNomeUsuario() {
+    var nomeUsuario = prompt("Por favor, digite seu nome:");
+    if (nomeUsuario != null && nomeUsuario != "") {
+        
+        var elementoH1 = document.querySelector('h1');
+        var elementoH2 = document.querySelector('h2');
+
+        elementoH1.textContent = "Olá " + nomeUsuario;
+        elementoH2.textContent = "Seja bem-vindo!";
+    }
+}
+
+window.onload = solicitarNomeUsuario;
