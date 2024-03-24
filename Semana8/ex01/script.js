e8b57fec15a777fd72074cf0623985629eeaad9e
Maycom Pires <maycompires@hotmail.com>
2024-03-24 14:29:24 -0300

    Ex 3 - Criar e Adicionar Elemento

--- Semana8/ex01/script.js
+++ Semana8/ex01/script.js
@@ -1,6 +1,10 @@
 const estiloH1 = document.getElementById('title');
     estiloH1.style.backgroundColor = 'red';
 
-const estiloClasse = document.querySelector('.minhaClasse');
+let estiloClasse = document.querySelector('.minhaClasse');
     estiloClasse.innerHTML = "Alterando texto de um elemento classe";
     estiloClasse.style.color = 'blue';
+
+let elementoCriado = document.createElement('p');
+    elementoCriado.appendChild(document.createTextNode('texto de p criado.'));
+    container.appendChild(elementoCriado);
