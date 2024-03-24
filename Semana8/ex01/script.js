5c59416d38080724399f98d6b402bd9499be74f6
Maycom Pires <maycompires@hotmail.com>
2024-03-24 14:01:10 -0300

    Ex 2 - Selecionar elemento por classe

--- Semana8/ex01/script.js
+++ Semana8/ex01/script.js
@@ -1,2 +1,6 @@
 const estiloH1 = document.getElementById('title');
-estiloH1.style.backgroundColor = 'red';
+    estiloH1.style.backgroundColor = 'red';
+
+const estiloClasse = document.querySelector('.minhaClasse');
+    estiloClasse.innerHTML = "Alterando texto de um elemento classe";
+    estiloClasse.style.color = 'blue';
