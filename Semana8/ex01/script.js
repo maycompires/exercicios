b7f637d34162a112574e4fcb2a23f8538b1c864e
Maycom Pires <maycompires@hotmail.com>
2024-03-26 23:01:47 -0300

    teste

--- Semana8/ex01/script.js
+++ Semana8/ex01/script.js
@@ -1,21 +1,21 @@
- let estiloH1 = document.getElementById('title');
-    estiloH1.style.backgroundColor = 'red';
+/*const estiloH1 = document.getElementById('title');
+estiloH1.style.backgroundColor = 'red';
 
-let estiloClasse = document.querySelector('.minhaClasse');
-    estiloClasse.innerHTML = "Alterando texto de um elemento classe";
-    estiloClasse.style.color = 'blue';
+const estiloClasse = document.querySelector('.minhaClasse');
+estiloClasse.innerHTML = "Alterando texto de um elemento classe";
+estiloClasse.style.color = 'blue';
 
-let elementoCriado = document.createElement('p'); 
-    elementoCriado.appendChild(document.createTextNode('texto de p criado.'));
-    container.appendChild(elementoCriado);
+const elementoCriado = document.createElement('p');
+elementoCriado.appendChild(document.createTextNode('texto de p criado.'));
+container.appendChild(elementoCriado);
 
-let container = document.querySelector("#container");
-let p = document.querySelector("#container p");
-    container.removeChild(p);
+const container = document.querySelector("#container");
+const p = document.querySelector("#container p");
+container.removeChild(p);
 
 let meuElemento = document.getElementById("meuElemento");
-    meuElemento.classList.add("novaClasse");
-    console.log(meuElemento);
+meuElemento.classList.add("novaClasse");
+console.log(meuElemento);*/
 
-let elemento = document.querySelectorAll('.minhaClasse')
-    elemento.style.color = 'red';
+let elementosClasses = document.querySelectorAll('.classeA');
+    elementosClasses.style.backgroundColor = 'yellow';
