a53c1cb5762e5ef5f9274b9cfec620671e597519
Maycom Pires <maycompires@hotmail.com>
2024-04-03 19:02:25 -0300

    Ex 5 - Alterar Estilo por Classe

--- Semana8/ex01/script.js
+++ Semana8/ex01/script.js
@@ -1,21 +1,25 @@
-/*const estiloH1 = document.getElementById('title');
-estiloH1.style.backgroundColor = 'red';
+// const estiloH1 = document.getElementById('title');
+// estiloH1.style.backgroundColor = 'red';
 
-const estiloClasse = document.querySelector('.minhaClasse');
-estiloClasse.innerHTML = "Alterando texto de um elemento classe";
-estiloClasse.style.color = 'blue';
+// const estiloClasse = document.querySelector('.minhaClasse');
+// estiloClasse.innerHTML = "Alterando texto de um elemento classe";
+// estiloClasse.style.color = 'blue';
 
-const elementoCriado = document.createElement('p');
-elementoCriado.appendChild(document.createTextNode('texto de p criado.'));
-container.appendChild(elementoCriado);
+// const elementoCriado = document.createElement('p');
+// elementoCriado.appendChild(document.createTextNode('texto de p criado.'));
+// container.appendChild(elementoCriado);
 
-const container = document.querySelector("#container");
-const p = document.querySelector("#container p");
-container.removeChild(p);
+// const container = document.querySelector("#container");
+// const p = document.querySelector("#container p");
+// container.removeChild(p);
 
-let meuElemento = document.getElementById("meuElemento");
-meuElemento.classList.add("novaClasse");
-console.log(meuElemento);*/
+// let meuElemento = document.getElementById("meuElemento");
+// meuElemento.classList.add("novaClasse");
+// console.log(meuElemento)
 
-let elementosClasses = document.querySelectorAll('.classeA');
-    elementosClasses.style.backgroundColor = 'yellow';
+
+var elementos = document.getElementsByClassName("Classe-paragrafo");
+        for (var i = 0; i < elementos.length; i++) {
+            elementos[i].style.color = "blue";
+            elementos[i].style.fontWeight = "normal";
+        }
